018d578fe9caf6977ae9befceea8b111950a8097
vcody <codyyv@protonmail.com>
2021-07-10 18:24:09 -0700

    Notes on Ch. 7, Dijkstra's Algorithm

---
+++ dijkstra.py
@@ -0,0 +1,9 @@
+'''
+Dijkstra's Algorithm steps:
+    1. Find "cheapest" node
+    2. Check if neighbors are cheaper. If yes, update
+    3. Repeat for every node
+    4. Calculate final path
+- Works with "directed acylic graphs"
+'''
+
