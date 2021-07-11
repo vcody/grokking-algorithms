61f0640890a2c2130e858f71c7b62124f23598ce
vcody <codyyv@protonmail.com>
2021-07-10 20:42:05 -0700

    Added Ch. 7 Notes, Dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -5,5 +5,69 @@ Dijkstra's Algorithm steps:
     3. Repeat for every node
     4. Calculate final path
 - Works with "directed acylic graphs"
+- Does not work with negative-weight edges
+    -> Use Bellman-Ford algorithm (todo)
 '''
 
+graph = {}
+
+graph['start'] = {}
+graph['start']['a'] = 6
+graph['start']['b'] = 2
+
+graph['a'] = {}
+graph['a']['fin'] = 1
+
+graph['b'] = {}
+graph['b']['a'] = 3
+graph['b']['fin'] = 5
+
+graph['fin'] = {}
+
+infinity = float('inf')
+costs = {}
+costs['a'] = 6
+costs['b'] = 2
+costs['fin'] = infinity
+
+parents = {}
+parents['a'] = 'start'
+parents['b'] = 'start'
+parents['fin'] = None
+
+processed= []
+
+# While we have nodes to process...
+# 1. Grab node closest to start
+# 2. Update costs for its neighbors
+# 3. If any neighbors costs were updated, update parents also
+# 4. Mark node as processed, repeat until finished
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float('inf')
+    lowest_cost_node = None
+
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+# Find cheapest node
+node = find_lowest_cost_node(costs)
+# Look through all nodes not processed yet
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    # Check all neighbors
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            # Update cost
+            costs[n] = new_cost
+            # Node becomes new parent for neighbor
+            parents[n] = node
+    processed.append(node)
+    # Find next node, repeat
+    node = find_lowest_cost_node(costs)
