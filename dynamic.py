e54d53456d0c35bb080940d81b4b679728771fa5
vcody <codyyv@protonmail.com>
2021-07-13 14:20:25 -0700

    Added Ch. 9, Dynamic Programming

---
+++ dynamic.py
@@ -0,0 +1,18 @@
+# Dynamic programming - solve sub-problems, building up to main problem
+# Row-ordering doesn't matter
+# e.g. Knapsack problem, git diff, DNA strand similarities, etc.
+
+# Exercises 
+# 9.1. No
+
+# Only works on discrete problems, i.e., doesn't depend on other subproblems
+
+# Exercises
+# 9.2. Camera, food, water
+
+# Useful for optimizing something given constraint
+
+# General dynamic programming tips:
+# 1. Each solution involves a grid 
+# 2. Values in cells = values being optimized
+# 3. Think about how to divide problem into subproblems
